3aaffd2de74acae7c870f81c29f187421b2e2837
Sergey Krasnikov <sergey.krasnikov@teligent.ru>
2014-04-14 19:05:59 +0400

    OCLIENT-89 new urls

--- src/osdk.config.js
+++ src/osdk.config.js
@@ -6,21 +6,21 @@
 
   // Default configuration
   oSDK.defaultConfig = {
-    apiServerURL: 'https://osdp-teligent-test-apigw.teligent.ru:8243', //TODO: replace this and following with sdk build parameter
-    credsURI: '/am/1.0.1/ephemerals',
+    apiServerURL: 'https://osdp-teligent-dev-apigw.virt.teligent.ru:8243', //TODO: replace this and following with sdk build parameter
+    credsURI: '/osdp/1.0.0/ephemerals',
     authURI: '/authorize/',
       //debug
     testScope: 'gold wood uranium', // FIXME: may be for us(wso2) its not needed?
     sip: {
       // TODO: move to sip module config
-      serverURL: 'wss://osdp-teligent-test-registrar.teligent.ru:8088/ws'
+      serverURL: 'wss://osdp-teligent-dev-registrar.virt.teligent.ru:8088/ws'
     },
     xmpp: {
       // TODO: move to xmpp module config
       /**
        * Inner JSJaC debuger
        */
-      debug: false,
+      debug: true,
       /**
        * IQ Out interval
        */
@@ -35,7 +35,7 @@
        */
       server: {
         protocol: 'wss',
-        domain: 'osdp-teligent-test-xmpp.teligent.ru',
+        domain: 'osdp-teligent-dev-xmpp.virt.teligent.ru',
         port: 5280,
         url: 'http-bind'
       }
